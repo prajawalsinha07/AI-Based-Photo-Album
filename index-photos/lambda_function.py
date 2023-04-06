44bdc8a8fa21aa6566b174f6407d980964e097dd
Prajawal Sinha <113229704+prajawalsinha07@users.noreply.github.com>
2023-04-06 18:34:28 -0400

    Update lambda_function.py

--- index-photos/lambda_function.py
+++ index-photos/lambda_function.py
@@ -6,6 +6,7 @@ import random
 from elasticsearch import Elasticsearch, RequestsHttpConnection
 from aws_requests_auth.aws_auth import AWSRequestsAuth
 
+
 s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
